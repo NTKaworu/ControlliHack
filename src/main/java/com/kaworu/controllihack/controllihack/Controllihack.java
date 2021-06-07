91d36d5666e1bf6f7fde910f94a2532fdd89c692
NTKaworu <mf.lollerino@gmail.com>
2021-06-07 18:05:19 +0200

    First Commit

--- src/main/java/com/kaworu/controllihack/controllihack/Controllihack.java
+++ src/main/java/com/kaworu/controllihack/controllihack/Controllihack.java
@@ -1,17 +1,18 @@
 package com.kaworu.controllihack.controllihack;
 
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Controllihack extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "[ControlliHack]: Plugin loaded!");
 
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        getServer().getConsoleSender().sendMessage(ChatColor.RED + "[ControlliHack]: Plugin unloaded!");
     }
 }
